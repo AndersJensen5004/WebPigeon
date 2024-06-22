7be06209f75ceb36f04ca494a06d088ab3e7bd70
Anders Jensen <jensen.anders.p@gmail.com>
2024-06-22 16:06:13 -0400

    css changes + JS

--- web-pigeon/src/Navbar.js
+++ web-pigeon/src/Navbar.js
@@ -1,14 +1,24 @@
 import {Link} from 'react-router-dom';
+import pigeonLogo from "./pigeon-logo.png";
+import letterLogo from "./letter-logo.png";
 
 const Navbar = () => {
     return (
+        <>
+        <div className="header">
+            <img src={letterLogo} alt="Letter P" />
+            version 1.0
+        </div>
         <nav className="navbar">
+            <img src={pigeonLogo} alt="Web Pigeon Logo"/>
             <h1>Web Pigeon</h1>
             <div className="links">
                 <Link to="/">Home</Link>
-                <Link to="/create" >Create Messenger</Link>
+                <Link to="/">Find</Link>
+                <Link to="/create" >Create</Link>
             </div>
         </nav>
+        </>
     );
 }
  
