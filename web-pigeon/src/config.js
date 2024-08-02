7c501645db9c328268c3bb33daca56bfaa7f9ea4
Anders Jensen <jensen.anders.p@gmail.com>
2024-08-02 12:49:31 -0400

    Fixing webhooks, live messages work now
    finally

--- web-pigeon/src/config.js
+++ web-pigeon/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl:"https://webpigeon.onrender.com"
+    //apiBaseUrl:"https://webpigeon.onrender.com"
+    apiBaseUrl:"http://127.0.0.1:5000"
 };
 
 export default config;
